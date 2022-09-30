93ac46f64c6299b0790bbf9b5ee57df719cc847a
SimSot <mksim001@gmail.com>
2022-09-30 08:59:04 +0200

    Timesheet properties and mappings

--- TimesheetImport.Infrastructure/Repository/ModelMappings/TimesheetSiteMapper.cs
+++ TimesheetImport.Infrastructure/Repository/ModelMappings/TimesheetSiteMapper.cs
@@ -1,9 +1,8 @@
 ﻿using ExcelDataReader;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TimesheetImport.Infrastructure.Repository.Models;
 using TimesheetImport.TimesheetModels;
 
@@ -62,9 +61,64 @@ namespace TimesheetImport.Infrastructure.Repository.ModelMappings
                                     Surname = reader.GetString(1),
                                     Name = reader.GetString(2),
                                     IdNumber = reader.GetValue(3)?.ToString(),
-                                    Start = sd,//Convert.ToDateTime(reader.GetString(j)),
-                                    End = sd.AddHours(rdEndDate)//Convert.ToDateTime(reader.GetString(j))
-                                });
+                                    StartDate = sd,
+                                    EndDate = sd.AddHours(rdEndDate),
+                                    CreatedBy = 1, //1,31,35,36,43,44,53 we need to know what this maps to. 
+                                    CreatedDate = DateTime.Now,
+                                    UpdatedBy = 1, // same as created by
+                                    UpdatedDate = DateTime.Now,
+                                    TimeStamp = DateTime.Now,
+                                    Deleted = null,
+                                    Secterr = null, // NULL,- 2147483640,- 1342177274
+                                    WorkflowId = null,
+                                    Status = null,// NULL,Approved,Duplicate,Leave,New,NightShift,Normal,UnApproved
+                                    CompanyId = null, // couple of companies
+                                    PersonId = null,
+                                    OpportunityId = null,
+                                    OrderId = null,
+                                    QuoteId = null, //99
+                                    LeadId = null,
+                                    CaseId = null,
+                                    normalhrs = rdEndDate, //verify this
+                                    overtimehrs = rdEndDate - 8, //anything over than 8 hours is overtime??
+                                    phhrs = 0,//don't this
+                                    nightshifthrs = null,
+                                    sundayhrs = DateTime.Now.DayOfWeek.ToString() == "Sunday" ? rdEndDate : null,
+                                    stage = null,
+                                    siteid = Convert.ToInt32(fileUploadRequest.SiteId),
+                                    prechargesheetid = null,
+                                    breaktimehrs = null, // how to calculate
+                                    normalhrstotal = null, // how to calculate
+                                    normalhrstotal_CID = null, // how to calculate
+                                    overtimehrstotal = null, // how to calculate
+                                    overtimehrstotal_CID = null, // how to calculate
+                                    position = null,
+                                    shift = shift == 6 ? "NormalShift" : "NightShift",
+                                    starttime = shift.ToString(),
+                                    endtime = shift == 6 ? "2pm" : "8pm",
+                                    approved = null, //Y,N
+                                    placeholder1 = null,
+                                    nightshifthrstotal = null,
+                                    nightshifthrstotal_CID = null, //  how to get this
+                                    employeeidsearch = 1, // how to get this
+                                    positionsearch = 1,// hot to get this
+                                    includedweekrun = null,
+                                    invoiced = null, 
+                                    workedhrs = rdEndDate,
+                                    startdatesearch = null,
+                                    enddatesearch = null,
+                                    Source = "Import", //CRM, Payrun
+                                    BatchNo = 1, // how to get this
+                                    calculatedhrs = null, // hot to get this
+                                    payrunid = 1, // how to get this
+                                    week = CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(DateTime.Now, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday),
+                                    timesheetrunid = 1,// how to get this
+                                    invoiceid = null,
+                                    calcnewtrainhrs = null, // how to get this
+                                    invoicerunid = null,
+                                    Override = null, //Y,N
+                                    newweek = CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(DateTime.Now, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday)
+                                }) ;
                             }
                             j += 4;
                             startDate = startDate.AddDays(1);
--- TimesheetImport.Infrastructure/Repository/Models/Timesheet.cs
+++ TimesheetImport.Infrastructure/Repository/Models/Timesheet.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TimesheetImport.Infrastructure.Repository.Models
 {
@@ -13,7 +9,65 @@ namespace TimesheetImport.Infrastructure.Repository.Models
         public string Name { get; set; }
         public string Surname { get; set; }
         public string IdNumber { get; set; }
-        public DateTime? Start { get; set; }
-        public DateTime? End { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public int? Deleted { get; set; }
+        public int? Secterr { get; set; }
+        public int? WorkflowId { get; set; }
+        public string? Status { get; set; }
+        public int? CompanyId { get; set; }
+        public int? PersonId { get; set; }
+        public int? OpportunityId { get; set; }
+        public int? OrderId { get; set; }
+        public int? QuoteId { get; set; }
+        public int? LeadId { get; set; }
+        public int? CaseId { get; set; }
+        public DateTime startdate { get; set; }
+        public DateTime enddate { get; set; }
+        public Double? normalhrs { get; set; }
+        public Double? overtimehrs { get; set; }
+        public Double? phhrs { get; set; }
+        public Double? nightshifthrs { get; set; }
+        public Double? sundayhrs { get; set; }
+        public string? stage { get; set; }
+        public int? siteid { get; set; }
+        public int? prechargesheetid { get; set; }
+        public Double? breaktimehrs { get; set; }
+        public Double? normalhrstotal { get; set; }
+        public int? normalhrstotal_CID { get; set; }
+        public int? overtimehrstotal { get; set; }
+        public int? overtimehrstotal_CID { get; set; }
+        public int? position { get; set; }
+        public string shift { get; set; }
+        public string? starttime { get; set; }
+        public string? endtime { get; set; }
+        public string? approved { get; set; }
+        public string? placeholder1 { get; set; }
+        public Double? nightshifthrstotal { get; set; }
+        public int? nightshifthrstotal_CID { get; set; }
+        public int employeeidsearch { get; set; }
+        public int positionsearch { get; set; }
+        public string? includedweekrun { get; set; }
+        public int? invoiced { get; set; }
+        public Double workedhrs { get; set; }
+        public DateTime? startdatesearch { get; set; }
+        public DateTime? enddatesearch { get; set; }
+        public string Source { get; set; }
+        public int BatchNo { get; set; }
+        public string calculatedhrs { get; set; }
+        public int payrunid { get; set; }
+        public int week { get; set; }
+        public int timesheetrunid { get; set; }
+        public int? invoiceid { get; set; }
+        public string? calcnewtrainhrs { get; set; }
+        public int? invoicerunid { get; set; }
+        public string Override { get; set; }
+        public int newweek { get; set; }
+
     }
 }
